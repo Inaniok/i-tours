31152c04034e54b9639d339a9ee160e16559f582
v1tal4ik <vetalgta@gmail.com>
2023-04-05 08:47:09 +0300

    push changes after first lesson

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/header';
+import Tours from './components/tours';
+
+const App = () => {
+	return (
+		<>
+			<Header></Header>
+			<Tours></Tours>
+		</>
+	);
+};
+
+export default App;
